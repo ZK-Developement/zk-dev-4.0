800c62075cbd1694794ce24dddd2abdc7d68a655
Ziomeczek <igordukiewicz@gmail.com>
2026-09-19 00:01:46 +0200

    Update oferta.jsx

--- zkdev4.0/src/components/oferta/oferta.jsx
+++ zkdev4.0/src/components/oferta/oferta.jsx
@@ -1,13 +1,15 @@
+
 function Oferta (){
     return(
-        <section className=" w-[1280px] h-[675px] flex justify-center items-center
+        <section className=" w-[1280px] h-[675px] flex justify-center items-center mt-[30px]
                             max-[1280px]:w-[1050px] 
                             max-[400px]:h-[550px]">
-            <div>
-                <dov>
-                    
-                </dov>
-
+            <div className="adaptive-bg bg-white w-[1220px] h-[675px] rounded-[20px] shadow-[0_4px_30px_rgba(0,0,0,0.10)]
+                                           flex items-center pl-[65px] bg-[length:100%_100%] bg-center
+                                           max-[1280px]:w-[1050px] 
+                                           max-[800px]:w-[740px] max-[800px]:pl-[40px]
+                                           max-[400px]:w-[340px] max-[400px]:pt-[100px] max-[400px]:h-[550px] max-[400px]:bg-[length:100%_100%] max-[400px]:bg-center max-[400px]:pl-[20px]" 
+                                           >
             </div>
         </section>
     );
