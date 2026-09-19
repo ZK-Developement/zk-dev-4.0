5c7fa9d974a531f0249bf1a4414212ec81754c0e
Ziomeczek <igordukiewicz@gmail.com>
2026-09-19 23:31:43 +0200

    Update footer.jsx

--- zkdev4.0/src/components/footer/footer.jsx
+++ zkdev4.0/src/components/footer/footer.jsx
@@ -6,7 +6,7 @@ function Footer (){
                     <div className="w-[333px] h-[242px] flex flex-col gap-[10px] p-[20px] text-black">
                         <p className="text-[10px] text-gray-500">KONTAKT</p>
                         <div className="text-[14px] ">
-                            <p className="h-[18px]">tel:  48+ 574 668 418</p>
+                            <p className="h-[18px]">tel:  48+ 791 203 396</p>
                             <p className="h-[18px]">e-mail: contactus.zk.business@gmail.com</p>
                         </div>
                         <div className="text-[14px] text-gray-600">
